ebfcdb9d603719ce7b5aeb08dbf14a6ed4ee4aef
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-12 15:09:01 -0500

    Add: some details to header component

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,3 +1,6 @@
+import { User } from 'lucide-react'
+import { Link } from 'react-router'
+
 interface Props {
   title: string
 }
@@ -5,12 +8,15 @@ interface Props {
 function Header({ title }: Props) {
   return (
     <header className="flex items-center justify-between mb-8 animate-fade-in">
-      <h1 className="text-2xl md:text-3xl font-bold uppercase tracking-tight text-foreground">
-        {title}
-      </h1>
+      <Link className="hover:underline" to="/">
+        <h1 className="text-2xl md:text-3xl font-bold uppercase tracking-tight text-foreground">
+          {title}
+        </h1>
+      </Link>
+
       <div className="flex items-center gap-2 text-foreground">
         <div className="h-8 w-8 rounded-full bg-secondary flex items-center justify-center">
-          {/* <User className="h-4 w-4 text-muted-foreground" /> */}
+          <User className="h-4 w-4 text-muted-foreground" />
         </div>
         <span className="text-sm font-medium hidden sm:block">Admin user</span>
       </div>
