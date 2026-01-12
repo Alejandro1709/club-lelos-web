6dbe7302486bed3db4164240cf5cd7173e2bf8ab
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-12 14:46:29 -0500

    Add: NotFoundPage

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router'
 import IndexPage from '../pages/IndexPage'
 import AppLayout from '../layouts/AppLayout'
+import NotFoundPage from '../pages/NotFoundPage'
 
 export const appRouter = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const appRouter = createBrowserRouter([
       {
         index: true,
         element: <IndexPage />,
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />,
       },
     ],
   },
