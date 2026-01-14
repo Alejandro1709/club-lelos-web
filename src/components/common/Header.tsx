f23496764a2310787c3ff5c20fe59c4fe38df10f
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-14 08:56:15 -0500

    Add: login button if no user

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -9,6 +9,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import { Button } from '../ui/button'
 
 interface Props {
   title: string
@@ -24,26 +25,32 @@ function Header({ title, user }: Props) {
         </h1>
       </Link>
 
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <div className="flex items-center gap-2 text-foreground cursor-pointer">
-            <div className="h-8 w-8 rounded-full bg-secondary flex items-center justify-center">
-              <User className="h-4 w-4 text-muted-foreground" />
+      {user ? (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="flex items-center gap-2 text-foreground cursor-pointer">
+              <div className="h-8 w-8 rounded-full bg-secondary flex items-center justify-center">
+                <User className="h-4 w-4 text-muted-foreground" />
+              </div>
+              <span className="text-sm font-medium hidden sm:block">
+                {user.name}
+              </span>
             </div>
-            <span className="text-sm font-medium hidden sm:block">
-              {user.name}
-            </span>
-          </div>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Role: {user.role}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ) : (
+        <div className="flex gap-2">
+          <Button>Login</Button>
+        </div>
+      )}
     </header>
   )
 }
